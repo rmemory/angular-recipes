442a04457473fe953293a4d8d742f36d58824414
Richard Memory <richardfmemory@gmail.com>
2018-05-04 17:26:38 -0600

    Finish the initial components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[]=[
-    new Recipe('A test recipe', 'this is a test description', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe('A test recipe', 'this is a test description', 'https://www.recipetineats.com/wp-content/uploads/2015/03/Honey-Garlic-Salmon-680x952.jpg')
   ];
 
   constructor() { }
